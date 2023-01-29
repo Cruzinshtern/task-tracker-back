9581e05a9cc8449aa433a0a85dbeb9d09d914171
Bogdan <bohdanmatv1@gmail.com>
2023-01-29 18:19:45 +0200

    config of variables with .env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => console.log('Server has started'));
+  await app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
 }
 bootstrap();
